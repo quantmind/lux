4e8a41b67996b4ae3b7d22ca49761410342bc4ed
Luca Sbardella <luca.sbardella@gmail.com>
2014-11-06 22:21:21 +0000

    added bs in javascript

--- luxjs/blog/blog-template.js
+++ luxjs/blog/blog-template.js
@@ -12,21 +12,23 @@ angular.module("blog/pagination.tpl.html", []).run(["$templateCache", function($
     "<ul class=\"media-list\">\n" +
     "    <li ng-repeat=\"post in items\" class=\"media\" data-ng-controller='BlogEntry'>\n" +
     "        <a href=\"{{post.html_url}}\">\n" +
-    "            <img ng-src=\"{{post.image}}\" class=\"hidden-xs post-image\" alt=\"{{post.title}}\">\n" +
-    "            <img ng-src=\"{{post.image}}\" alt=\"{{post.title}}\" class=\"visible-xs post-image-xs center-block\">\n" +
-    "            <div class=\"post-body hidden-xs\">\n" +
-    "                <h3 class=\"media-heading\">{{post.title}}</h3>\n" +
-    "                <p data-ng-if=\"post.description\">{{post.description}}</p>\n" +
-    "                <p class=\"text-info small\">by {{post.authors}} on {{post.dateText}}</p>\n" +
+    "            <div class=\"clearfix\">\n" +
+    "                <img ng-src=\"{{post.image}}\" class=\"hidden-xs post-image\" alt=\"{{post.title}}\">\n" +
+    "                <img ng-src=\"{{post.image}}\" alt=\"{{post.title}}\" class=\"visible-xs post-image-xs center-block\">\n" +
+    "                <div class=\"post-body hidden-xs\">\n" +
+    "                    <h3 class=\"media-heading\">{{post.title}}</h3>\n" +
+    "                    <p data-ng-if=\"post.description\">{{post.description}}</p>\n" +
+    "                    <p class=\"text-info small\">by {{post.authors}} on {{post.dateText}}</p>\n" +
+    "                </div>\n" +
+    "                <div class=\"visible-xs\">\n" +
+    "                    <br>\n" +
+    "                    <h3 class=\"media-heading text-center\">{{post.title}}</h3>\n" +
+    "                    <p data-ng-if=\"post.description\">{{post.description}}</p>\n" +
+    "                    <p class=\"text-info small\">by {{post.authors}} on {{post.dateText}}</p>\n" +
+    "                </div>\n" +
     "            </div>\n" +
-    "            <div class=\"visible-xs\">\n" +
-    "                <br>\n" +
-    "                <h3 class=\"media-heading text-center\">{{post.title}}</h3>\n" +
-    "                <p data-ng-if=\"post.description\">{{post.description}}</p>\n" +
-    "                <p class=\"text-info small\">by {{post.authors}} on {{post.dateText}}</p>\n" +
-    "            </div>\n" +
+    "            <hr>\n" +
     "        </a>\n" +
-    "        <hr>\n" +
     "    </li>\n" +
     "</ul>");
 }]);
