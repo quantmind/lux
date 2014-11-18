08a4a0fb0c53086e6ec15fae11bb8593b04fff94
Luca Sbardella <luca.sbardella@gmail.com>
2014-11-18 11:55:26 +0000

    added pick in js

--- lux/media/lux/lux.js
+++ lux/media/lux/lux.js
@@ -99,6 +99,20 @@ function(angular, root) {
         }]);
 
     lux.context.ngModules.push('lux.applications');
+    var _ = lux._ = {},
+
+    pick = _.pick = function (obj, callback) {
+        var picked = {},
+            val;
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                val = callback(obj[key], key);
+                if (val !== undefined)
+                    picked[key] = val;
+            }
+        }
+        return picked;
+    };
     var
     //
     ostring = Object.prototype.toString,
---
+++ luxjs/underscore.js
@@ -0,0 +1,14 @@
+    var _ = lux._ = {},
+
+    pick = _.pick = function (obj, callback) {
+        var picked = {},
+            val;
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                val = callback(obj[key], key);
+                if (val !== undefined)
+                    picked[key] = val;
+            }
+        }
+        return picked;
+    };
