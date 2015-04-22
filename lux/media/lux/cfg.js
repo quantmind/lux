78fda2e869081366fb3fd3699bf5b9d32c066651
Luca Sbardella <luca.sbardella@gmail.com>
2015-04-22 08:08:45 +0100

    http as default protocol in cfg.js

--- lux/media/lux/cfg.js
+++ lux/media/lux/cfg.js
@@ -5,8 +5,8 @@
     if (!root.lux)
         root.lux = {};
 
-    // The original require
-    var protocol = root.location ? (root.location.protocol === 'file:' ? 'https:' : '') : '',
+    // If a file assign http as protocol (https does not work with PhantomJS)
+    var protocol = root.location ? (root.location.protocol === 'file:' ? 'http:' : '') : '',
         end = '.js',
         ostring = Object.prototype.toString,
         lux = root.lux;
--- lux/media/lux/lux.js
+++ lux/media/lux/lux.js
@@ -1,6 +1,6 @@
 //      Lux Library - v0.1.1
 
-//      Compiled 2015-04-21.
+//      Compiled 2015-04-22.
 //      Copyright (c) 2015 - Luca Sbardella
 //      Licensed BSD.
 //      For all details and documentation:
