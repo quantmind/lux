b59140e045e21e784089910115bd13ef0a8987c2
tazo <tazo90@gmail.com>
2015-05-20 16:04:38 +0200

    ui-grid integration with API

--- luxjs/grid/module.js
+++ luxjs/grid/module.js
@@ -1,77 +1,53 @@
-/*jshint sub:true*/
-
+    //
+    //  Module for interacting with API to fetch model data and display it
     angular.module('lux.grid', ['ngTouch', 'ui.grid'])
+        //
+        .service('gridService', ['$lux', function ($lux, $http) {
 
-        .service('gridService', ['$lux', function ($lux) {
-
-            this.api_mock_data = {
-                'user': {
-                    'columns': [
-                        {field: 'id', displayName: 'ID'},
-                        {field: 'user', displayName: 'user'},
-                        {field: 'edit', cellTemplate: '<div><button class="btn btn-primary" ng-click=" getExternalScopes().onClick(row.entity.fullName)">Edit</button></div>'}
-                    ],
-                    'rows': [{
-                        'id': 1,
-                        'user': 'Marius',
-                    }, {
-                        'id': 2,
-                        'user': 'Adam',
-                    }]
-                },
-                'group': {
-                    'columns': [
-                        {field: 'id', displayName: 'ID'},
-                        {field: 'group', displayName: 'Group'}
-                    ],
-                    'rows': [{
-                        'id': 1,
-                        'group': 'Admins',
-                    }, {
-                        'id': 2,
-                        'group': 'Staff',
-                    }]
-                },
-                'exchange': {
-                    'columns': [
-                        {field: 'id', displayName: 'ID'},
-                        {field: 'exchange', displayName: 'Exchange'},
-                        {field: 'price', displayName: 'Price'}
-                    ],
-                    'rows': [{
-                        'id': 1,
-                        'exchange': 'Admins',
-                        'price': 100,
-                    }, {
-                        'id': 2,
-                        'exchange': 'Staff',
-                        'price': 200,
-                    }]
-                }
+            String.prototype.capitalize = function() {
+                return this.charAt(0).toUpperCase() + this.slice(1);
             };
 
-            this.getModelData = function() {
+            this.buildColumnDefs = function(fields) {
+                var columns = [];
+
+                angular.forEach(fields, function(_, key) {
+                    columns.push({
+                        field: key,
+                        displayName: key.split('_').join(' ').capitalize()
+                    });
+                });
+
+                return columns;
+            };
+
+            this.getModelUrl = function() {
                 var loc = window.location,
                     path = loc.pathname,
-                    model = path.split('/').pop(-1),
-                    modelDefs;
+                    modelname = path.split('/').pop(-1);
 
-                if (this.api_mock_data.hasOwnProperty(model)) {
-                    modelDefs = this.api_mock_data[model];
-                    return modelDefs;
-                }
+                return modelname + '_url';
             };
 
         }])
-
+        //
         .controller('RestGrid', ['$scope', '$lux', 'gridService', function (scope, $lux, gridService) {
 
-            var client = scope.api(),
-                gridData = gridService.getModelData();
+            var modelUrl = gridService.getModelUrl(),
+                client = scope.api(),
+                target = {
+                    url: scope.API_URL,
+                    name: modelUrl
+                };
 
             scope.gridOptions = {
-                data: gridData.rows,
-                columnDefs: gridData.columns
+                data: [],
+                columnDefs: []
             };
+
+            client.get(target).success(function(data) {
+                scope.gridOptions.data = data;
+                scope.gridOptions.columnDefs = gridService.buildColumnDefs(data[0]);
+            });
 
         }]);
