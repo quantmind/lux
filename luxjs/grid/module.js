8dca49871ed27c318a49ad199e45cadeb2c30dcc
tazo <tazo90@gmail.com>
2015-05-21 19:09:14 +0200

    ui-grid integration

--- luxjs/grid/module.js
+++ luxjs/grid/module.js
@@ -1,20 +1,98 @@
 
-    angular.module('lux.grid', ['ngTouch', 'ui.grid'])
+    angular.module('lux.grid', ['ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.selection'])
 
         // Directive to build Angular-UI grid options using Lux REST API
-        .directive('restGrid', ['$lux', function ($lux) {
+        .directive('restGrid', ['$lux', '$window', function ($lux, $window) {
 
-            // Pre-process grid options
-            function buildOptions (options) {
+            var paginationSize = 25;
 
-                var api = $lux.api(options.target),
-                    columns = options.columns;
+            // Get initial data
+            function getInitialData (scope, options) {
 
-                return {
-                    columnDefs: []
-                };
+                var api = $lux.api(options.target);
+
+                api.get({path: '/metadata'}).success(function(resp) {
+                    paginationSize = resp['default-limit'];
+
+                    api.get({}, {limit:paginationSize}).success(function(resp) {
+                        scope.gridOptions.totalItems = resp.total;
+                        scope.gridOptions.data = resp.result;
+                    });
+                });
             }
 
+            // Get specified page
+            function getPage (scope, options, pageNumber) {
+
+                var api = $lux.api(options.target),
+                    params = {
+                        limit: paginationSize,
+                        offset: paginationSize*(pageNumber - 1)
+                    };
+
+                api.get(options.target, params).success(function(resp) {
+                    scope.gridOptions.totalItems = resp.total;
+                    scope.gridOptions.data = resp.result;
+                });
+            }
+
+            // Pre-process grid options
+            function buildOptions (scope, options) {
+
+                scope.objectUrl = function(objectId) {
+                    return $window.location + '/' + objectId;
+                };
+
+                function getColumnType(type) {
+                    switch (type) {
+                        case 'integer':     return 'number';
+                        case 'datetime':    return 'date';
+                        default:            return type;
+                    }
+                }
+
+                var api = $lux.api(options.target),
+                    columns = [],
+                    gridOptions = {
+                        paginationPageSizes: [paginationSize],
+                        paginationPageSize: paginationSize,
+                        useExternalPagination: true,
+                        enableFiltering: true,
+                        columnDefs: [],
+                        rowHeight: 30,
+                        onRegisterApi: function(gridApi) {
+                            scope.gridApi = gridApi;
+                            scope.gridApi.pagination.on.paginationChanged(scope, function(currentPage) {
+                                getPage(scope, options, currentPage);
+                            });
+
+                            angular.forEach(options.columns, function(col) {
+                                var column = {
+                                    field: col.field,
+                                    displayName: col.displayName,
+                                    type: getColumnType(col.type),
+                                    enableSorting: col.sortable,
+                                    enableFiltering: col.filter,
+                                };
+
+                                if (column.field === 'id')
+                                    column.cellTemplate = '<div class="ui-grid-cell-contents"><a ng-href="{{grid.appScope.objectUrl(COL_FIELD)}}">{{COL_FIELD}}</a></div>';
+
+                                if (column.type === 'date') {
+                                    column.sortingAlgorithm = function(a, b) {
+                                        var dt1 = new Date(a.replace(/"/g, "")).getTime(),
+                                            dt2 = new Date(b.replace(/"/g, "")).getTime();
+                                        return dt1 === dt2 ? 0 : (dt1 < dt2 ? -1 : 1);
+                                    };
+                                }
+
+                                gridOptions.columnDefs.push(column);
+                            });
+                        }
+                    };
+
+                return gridOptions;
+            }
 
             return {
                 restrict: 'A',
@@ -32,9 +110,11 @@
                         opts = getOptions(opts);
 
                         if (opts)
-                            scope.gridOptions = buildOptions(opts);
-                    }
-                }
+                            scope.gridOptions = buildOptions(scope, opts);
+
+                        getInitialData(scope, opts);
+                    },
+                },
             };
 
         }]);
