a3328ad1730f54653f55aa2a4df079d21ef8ece6
tazo <tazo90@gmail.com>
2015-05-31 22:44:10 +0200

    added select for boolean fields and server side sorting

--- luxjs/grid/module.js
+++ luxjs/grid/module.js
@@ -2,7 +2,7 @@
     angular.module('lux.grid', ['ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.selection'])
 
         // Directive to build Angular-UI grid options using Lux REST API
-        .directive('restGrid', ['$lux', '$window', function ($lux, $window) {
+        .directive('restGrid', ['$lux', '$window', 'uiGridConstants', function ($lux, $window, uiGridConstants) {
 
             var paginationSize = 25;
 
@@ -22,16 +22,22 @@
             }
 
             // Get specified page
-            function getPage (scope, options, pageNumber) {
+            function getPage (scope, api, pageNumber) {
 
-                var api = $lux.api(options.target),
-                    params = {
-                        limit: paginationSize,
-                        offset: paginationSize*(pageNumber - 1)
-                    };
+                var params = {
+                    limit: paginationSize,
+                    offset: paginationSize*(pageNumber - 1)
+                };
 
-                api.get(options.target, params).success(function(resp) {
+                api.get({}, params).success(function(resp) {
                     scope.gridOptions.totalItems = resp.total;
+                    scope.gridOptions.data = resp.result;
+                });
+            }
+
+            // Get current page using query string
+            function getPageByParams(scope, api, params) {
+                api.get({}, params).success(function(resp) {
                     scope.gridOptions.data = resp.result;
                 });
             }
@@ -57,13 +63,39 @@
                         paginationPageSizes: [paginationSize],
                         paginationPageSize: paginationSize,
                         useExternalPagination: true,
+                        useExternalSorting: true,
                         enableFiltering: true,
                         columnDefs: [],
                         rowHeight: 30,
                         onRegisterApi: function(gridApi) {
                             scope.gridApi = gridApi;
                             scope.gridApi.pagination.on.paginationChanged(scope, function(currentPage) {
-                                getPage(scope, options, currentPage);
+                                getPage(scope, api, currentPage);
+                            });
+
+                            scope.gridApi.core.on.sortChanged(scope, function(grid, sortColumns) {
+                                var params = {};
+
+                                // Build query string for sorting
+                                angular.forEach(sortColumns, function(column) {
+                                    params.sortby = column.name + ':' + column.sort.direction;
+                                });
+
+                                if( sortColumns.length === 0)
+                                    getPageByParams(scope, api, params);
+                                else {
+                                    switch( sortColumns[0].sort.direction ) {
+                                        case uiGridConstants.ASC:
+                                            getPageByParams(scope, api, params);
+                                            break;
+                                        case uiGridConstants.DESC:
+                                            getPageByParams(scope, api, params);
+                                            break;
+                                        case undefined:
+                                            getPageByParams(scope, api, params);
+                                            break;
+                                    }
+                                }
                             });
 
                             angular.forEach(options.columns, function(col) {
@@ -84,8 +116,14 @@
                                             dt2 = new Date(b).getTime();
                                         return dt1 === dt2 ? 0 : (dt1 < dt2 ? -1 : 1);
                                     };
-                                } else if (column.type === 'boolean')
+                                } else if (column.type === 'boolean') {
                                     column.cellTemplate = '<div class="ui-grid-cell-contents"><i ng-class="{{COL_FIELD == true}} ? \'fa fa-check-circle text-success\' : \'fa fa-times-circle text-danger\'"></i></div>';
+
+                                    column.filter = {
+                                        type: uiGridConstants.filter.SELECT,
+                                        selectOptions: [{ value: 'true', label: 'True' }, { value: 'false', label: 'False'}],
+                                    };
+                                }
 
                                 gridOptions.columnDefs.push(column);
                             });
