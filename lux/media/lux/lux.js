74362a8c25e31034838e979fe2c2a8f6958eedaf
Luca Sbardella <luca.sbardella@gmail.com>
2015-06-02 17:27:20 +0100

    merged grid

--- lux/media/lux/lux.js
+++ lux/media/lux/lux.js
@@ -1,6 +1,6 @@
 //      Lux Library - v0.2.0
 
-//      Compiled 2015-06-01.
+//      Compiled 2015-06-02.
 //      Copyright (c) 2015 - Luca Sbardella
 //      Licensed BSD.
 //      For all details and documentation:
@@ -1480,7 +1480,12 @@ angular.module("page/breadcrumbs.tpl.html", []).run(["$templateCache", function(
             return this;
         };
 
-        // Setup $stateProvider
+        //
+        //  Setup $stateProvider
+        //  =========================
+        //
+        //  This method should be called by the application, once it has setup
+        //  all the states via the ``state`` method.
         this.setup = function () {
             //
             if (pages) {
@@ -2369,9 +2374,12 @@ angular.module("page/breadcrumbs.tpl.html", []).run(["$templateCache", function(
     angular.module('lux.grid', ['ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.selection'])
 
         // Directive to build Angular-UI grid options using Lux REST API
-        .directive('restGrid', ['$lux', '$window', function ($lux, $window) {
+        .directive('restGrid', ['$lux', '$window', 'uiGridConstants', function ($lux, $window, uiGridConstants) {
 
-            var paginationSize = 25;
+            var paginationOptions = {
+                limit: 25,
+                sizes: [25, 50, 100],
+            };
 
             // Get initial data
             function getInitialData (scope, options) {
@@ -2379,9 +2387,9 @@ angular.module("page/breadcrumbs.tpl.html", []).run(["$templateCache", function(
                 var api = $lux.api(options.target);
 
                 api.get({path: '/metadata'}).success(function(resp) {
-                    paginationSize = resp['default-limit'];
+                    paginationOptions.limit = resp['default-limit'];
 
-                    api.get({}, {limit:paginationSize}).success(function(resp) {
+                    api.get({}, {limit: paginationOptions.limit}).success(function(resp) {
                         scope.gridOptions.totalItems = resp.total;
                         scope.gridOptions.data = resp.result;
                     });
@@ -2389,16 +2397,22 @@ angular.module("page/breadcrumbs.tpl.html", []).run(["$templateCache", function(
             }
 
             // Get specified page
-            function getPage (scope, options, pageNumber) {
+            function getPage (scope, api, pageNumber, pageSize) {
 
-                var api = $lux.api(options.target),
-                    params = {
-                        limit: paginationSize,
-                        offset: paginationSize*(pageNumber - 1)
-                    };
+                var params = {
+                    limit: pageSize,
+                    offset: pageSize*(pageNumber - 1)
+                };
 
-                api.get(options.target, params).success(function(resp) {
+                api.get({}, params).success(function(resp) {
                     scope.gridOptions.totalItems = resp.total;
+                    scope.gridOptions.data = resp.result;
+                });
+            }
+
+            // Get current page using query string
+            function getPageByParams(scope, api, params) {
+                api.get({}, params).success(function(resp) {
                     scope.gridOptions.data = resp.result;
                 });
             }
@@ -2421,16 +2435,42 @@ angular.module("page/breadcrumbs.tpl.html", []).run(["$templateCache", function(
                 var api = $lux.api(options.target),
                     columns = [],
                     gridOptions = {
-                        paginationPageSizes: [paginationSize],
-                        paginationPageSize: paginationSize,
+                        paginationPageSizes: paginationOptions.sizes,
+                        paginationPageSize: paginationOptions.limit,
                         useExternalPagination: true,
+                        useExternalSorting: true,
                         enableFiltering: true,
                         columnDefs: [],
                         rowHeight: 30,
                         onRegisterApi: function(gridApi) {
                             scope.gridApi = gridApi;
-                            scope.gridApi.pagination.on.paginationChanged(scope, function(currentPage) {
-                                getPage(scope, options, currentPage);
+                            scope.gridApi.pagination.on.paginationChanged(scope, function(pageNumber, pageSize) {
+                                getPage(scope, api, pageNumber, pageSize);
+                            });
+
+                            scope.gridApi.core.on.sortChanged(scope, function(grid, sortColumns) {
+                                var params = {};
+
+                                // Build query string for sorting
+                                angular.forEach(sortColumns, function(column) {
+                                    params.sortby = column.name + ':' + column.sort.direction;
+                                });
+
+                                if( sortColumns.length === 0)
+                                    getPageByParams(scope, api, params);
+                                else {
+                                    switch( sortColumns[0].sort.direction ) {
+                                        case uiGridConstants.ASC:
+                                            getPageByParams(scope, api, params);
+                                            break;
+                                        case uiGridConstants.DESC:
+                                            getPageByParams(scope, api, params);
+                                            break;
+                                        case undefined:
+                                            getPageByParams(scope, api, params);
+                                            break;
+                                    }
+                                }
                             });
 
                             angular.forEach(options.columns, function(col) {
@@ -2451,8 +2491,14 @@ angular.module("page/breadcrumbs.tpl.html", []).run(["$templateCache", function(
                                             dt2 = new Date(b).getTime();
                                         return dt1 === dt2 ? 0 : (dt1 < dt2 ? -1 : 1);
                                     };
-                                } else if (column.type === 'boolean')
+                                } else if (column.type === 'boolean') {
                                     column.cellTemplate = '<div class="ui-grid-cell-contents"><i ng-class="{{COL_FIELD == true}} ? \'fa fa-check-circle text-success\' : \'fa fa-times-circle text-danger\'"></i></div>';
+
+                                    column.filter = {
+                                        type: uiGridConstants.filter.SELECT,
+                                        selectOptions: [{ value: 'true', label: 'True' }, { value: 'false', label: 'False'}],
+                                    };
+                                }
 
                                 gridOptions.columnDefs.push(column);
                             });
