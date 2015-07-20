1c2b2480a6f9fcafe10973a6b85a0979e425889c
Luca Sbardella <luca.sbardella@gmail.com>
2015-07-20 12:58:30 +0100

    unified inner template behaviour

--- lux/forms/views.py
+++ lux/forms/views.py
@@ -14,9 +14,6 @@ def method_not_allowed(request):
 class WebFormRouter(HtmlRouter):
     '''A Router for rending web forms
     '''
-    uirouter = False
-    template = None
-
     form_method = None
     form_enctype = None
     form_action = None
@@ -34,9 +31,6 @@ class WebFormRouter(HtmlRouter):
         method = self.form_method or 'post'
         enctype = self.form_enctype or 'multipart/form-data'
         action = self.form_action or request.full_path()
-        html = form.as_form(action=action,
+        return form.as_form(action=action,
                             enctype=enctype,
                             method=method)
-        context = {'form': html.render(request)}
-        return request.app.render_template(self.template, context,
-                                           request=request)
