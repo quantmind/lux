1a63e541d1da3b4448761951504f710b60f9110d
tazo <tazo90@gmail.com>
2015-07-22 16:26:46 +0200

    update navbar

--- luxjs/nav/nav.js
+++ luxjs/nav/nav.js
@@ -91,10 +91,23 @@
             this.maybeCollapse = function (navbar) {
                 var width = window.innerWidth > 0 ? window.innerWidth : screen.width,
                     c = navbar.collapse;
+
                 if (width < navbar.collapseWidth)
                     navbar.collapse = 'collapse';
                 else
                     navbar.collapse = '';
+                return c !== navbar.collapse;
+            };
+
+            this.collapseForWide = function(navbar, element) {
+                var width = window.innerWidth > 0 ? window.innerWidth : screen.width,
+                    c = navbar.collapse;
+
+                if (width > navbar.collapseWidth) {
+                    // If dropdown was opened then collapse
+                    if (element.find('nav')[1].classList.contains('in'))
+                        navbar.collapse = 'collapse';
+                }
                 return c !== navbar.collapse;
             };
         }])
@@ -116,6 +129,11 @@
                 // Link function
                 link: function (scope, element, attrs) {
                     navService.initScope(scope, attrs);
+                    //
+                    windowResize(function () {
+                        if (navService.collapseForWide(scope.navbar, element))
+                            scope.$apply();
+                    });
                     //
                     // When using ui-router, and a view changes collapse the
                     //  navigation if needed
