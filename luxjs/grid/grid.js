61ee9b55af61fd0ffc7c81b5de8360363a0adfe0
Jeremy Herr <jeremyherr@bmlltech.com>
2015-08-28 15:41:20 +0100

    Issue #110 Added modal to display contents of lux.grid cell

--- luxjs/grid/grid.js
+++ luxjs/grid/grid.js
@@ -309,6 +309,18 @@
 
             // Get initial data
             this.getInitialData = function(scope, connectionType, gridConfig) {
+                $templateCache.put('lux/grid/record-details', '<div class="modal" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog record-details"><div class="modal-content"><div class="modal-header" ng-show="title"><button type="button" class="close" aria-label="Close" ng-click="$hide()"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" ng-bind="title"></h4></div><div class="modal-body" ><pre ng-bind="content"></pre></div><div class="modal-footer"><button type="button" class="btn btn-default" ng-click="$hide()">Close</button></div></div></div></div>');
+
+                scope.popup = function(id, content) {
+                    $modal({
+                        "title": "Details for " + id,
+                        "content": content,
+                        "placement": "center",
+                        "backdrop": true,
+                        "template": 'lux/grid/record-details'
+                    });
+                };
+
                 function onMetadataReceived(metadata) {
                     scope.gridState.limit = metadata['default-limit'];
                     scope.gridOptions.metaFields = {
