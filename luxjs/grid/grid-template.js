9c81464e01f055441e1156916a2fe16f7a6e9fc2
Jeremy Herr <jeremyherr@bmlltech.com>
2015-09-03 11:30:29 +0100

    Issue #110 Moved template into its own tpl.html file

--- luxjs/grid/grid-template.js
+++ luxjs/grid/grid-template.js
@@ -1,4 +1,4 @@
-angular.module('templates-grid', ['grid/templates/modal.columns.tpl.html', 'grid/templates/modal.delete.tpl.html', 'grid/templates/modal.empty.tpl.html']);
+angular.module('templates-grid', ['grid/templates/modal.columns.tpl.html', 'grid/templates/modal.delete.tpl.html', 'grid/templates/modal.empty.tpl.html', 'grid/templates/modal.record.details.tpl.html']);
 
 angular.module("grid/templates/modal.columns.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("grid/templates/modal.columns.tpl.html",
@@ -69,6 +69,29 @@ angular.module("grid/templates/modal.empty.tpl.html", []).run(["$templateCache",
     "      </div>\n" +
     "    </div>\n" +
     "  </div>\n" +
+    "</div>\n" +
+    "");
+}]);
+
+angular.module("grid/templates/modal.record.details.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("grid/templates/modal.record.details.tpl.html",
+    "<div class=\"modal\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">\n" +
+    "    <div class=\"modal-dialog record-details\">\n" +
+    "        <div class=\"modal-content\">\n" +
+    "            <div class=\"modal-header\" ng-show=\"title\">\n" +
+    "                <button type=\"button\" class=\"close\" aria-label=\"Close\" ng-click=\"$hide()\">\n" +
+    "                    <span aria-hidden=\"true\">&times;</span>\n" +
+    "                </button>\n" +
+    "                <h4 class=\"modal-title\" ng-bind=\"title\"></h4>\n" +
+    "            </div>\n" +
+    "            <div class=\"modal-body\" >\n" +
+    "                <pre ng-bind=\"content\"></pre>\n" +
+    "            </div>\n" +
+    "            <div class=\"modal-footer\">\n" +
+    "                <button type=\"button\" class=\"btn btn-default\" ng-click=\"$hide()\">Close</button>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
     "</div>\n" +
     "");
 }]);
