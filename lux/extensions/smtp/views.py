2fb77498fdd947df88bf5f8c822e7fceabdeeeaa
Jeremy Herr <jeremyherr@bmlltech.com>
2015-10-09 17:45:51 +0100

    Issue #144 ContactRouter now sends email according to Parameter ENQUIRY_EMAILS
     - Added ENQUIRY_EMAILS Parameter to app.py
     - ContactRouter sends an email for each entry in the array
       ENQUIRY_EMAILS.

--- lux/extensions/smtp/views.py
+++ lux/extensions/smtp/views.py
@@ -4,16 +4,13 @@ from lux.forms import Layout, Fieldset, Submit
 
 from pulsar.apps.wsgi import Json
 
-
 class ContactForm(forms.Form):
     """
     The base contact form class from which all contact form classes
     should inherit.
     """
-    name = forms.CharField(max_length=100,
-                           label='Your name')
-    email = forms.EmailField(max_length=200,
-                             label='Your email address')
+    name = forms.CharField(max_length=100, label='Your name')
+    email = forms.EmailField(max_length=200, label='Your email address')
     body = forms.TextField(label='Your message', rows=10)
 
 
@@ -23,7 +20,6 @@ HtmlContactForm = Layout(ContactForm,
 
 
 class ContactRouter(lux.HtmlRouter):
-
     def get_html(self, request):
         url = str(self.full_route)
         return HtmlContactForm(request).as_form(action=url)
@@ -33,12 +29,16 @@ class ContactRouter(lux.HtmlRouter):
         form = ContactForm(request, data=data)
         if form.is_valid():
             email = request.app.email_backend
-            subject = 'Message from %s' % request.config['APP_NAME']
-            email.send_mail(to=form.cleaned_data['email'],
-                            subject=subject,
-                            message=form.cleaned_data['body'])
-            data = dict(success=True,
-                        message="Message sent")
+
+            for emailSettings in request.app.config['ENQUIRY_EMAILS']:
+                emailFields = {
+                    tag: emailSettings[tag].format(**form.cleaned_data)
+                          for tag in ['sender', 'to', 'subject', 'message']
+                }
+                email.send_mail(**emailFields)
+
+            data = dict(success=True, message="Message sent")
+
         else:
             data = form.tojson()
         return Json(data).http_response(request)
