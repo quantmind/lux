43de0868897a19f5a7b3dd795c2bb3358036edfe
Jeremy Herr <jeremyherr@bmlltech.com>
2015-10-14 11:27:37 +0100

    Issue #144 ContactRouter unit test now covers the actual guts of the mail sending functionality

--- tests/views/views.py
+++ tests/views/views.py
@@ -1,6 +1,6 @@
 import unittest
 import lux.extensions.smtp.views as views
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, PropertyMock
 
 
 class EmptyClass:
@@ -30,17 +30,29 @@ class ContactRouterTestCase(unittest.TestCase):
         views.HtmlContactForm.assert_called_once_with(emptyRequest)
         mockForm.as_form.assert_called_once_with(action=dummyRule)
 
-    def test_post_empty_emails(self):
+    def test_post_one_email_form_valid(self):
         with patch('lux.core.wrappers.WsgiRequest') as WsgiRequestMock, patch(
                 'lux.extensions.smtp.views.Json') as JsonMock:
             requestMock = WsgiRequestMock()
             requestMock.data_and_files.return_value = ({}, '')
 
-            requestMock.app.config['ENQUIRY_EMAILS'] = []
+            requestMock.app.config = dict(ENQUIRY_EMAILS=[
+                {
+                    'sender': 'BMLL Technologies <noreply@bmlltech.com>',
+                    'to': 'info@bmlltech.com',
+                    'subject': 'website enquiry form',
+                    'message': 'Enquiry from: {name} <{email}>\n\n' \
+                               + 'Message:\n' \
+                               + '{body}\n'
+                },
+            ])
 
             formMock = MagicMock()
             FormMock = MagicMock()
             formMock.is_valid.return_value = True
+            formMock.cleaned_data = dict(name='Jeremy Herr',
+                                         email='jeremyherr@bmlltech.com',
+                                         body='Here is my message to you')
             FormMock.return_value = formMock
             views.ContactForm = FormMock
 
@@ -51,8 +63,18 @@ class ContactRouterTestCase(unittest.TestCase):
 
             self.cr.post(requestMock)
 
+            requestMock.app.email_backend.send_mail.assert_called_once_with(
+                sender='BMLL Technologies <noreply@bmlltech.com>',
+                to='info@bmlltech.com',
+                subject='website enquiry form',
+                message='Enquiry from: Jeremy Herr <jeremyherr@bmlltech.com>\n\n' \
+                        + 'Message:\n' \
+                        + 'Here is my message to you\n'
+            )
+
             JsonMock.assert_called_once_with(
                 dict(success=True, message="Message sent"))
+            stringMock.http_response.assert_called_once_with(requestMock)
 
 
 if __name__ == '__main__':
