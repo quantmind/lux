b2bbf1040a5aecb6a169886cb21ec64834ed5675
Luca Sbardella <luca@quantmind.com>
2015-11-02 12:43:52 +0000

    test fix

--- examples/luxsite/media/luxsite/luxsite.js
+++ examples/luxsite/media/luxsite/luxsite.js
@@ -1,6 +1,6 @@
 //      Lux Library - v0.2.0
 
-//      Compiled 2015-10-19.
+//      Compiled 2015-11-02.
 //      Copyright (c) 2015 - Luca Sbardella
 //      Licensed BSD.
 //      For all details and documentation:
