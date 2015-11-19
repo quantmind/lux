c178befb81cd5804c8b864c92680600afc28a69a
tazo <tazo90@gmail.com>
2015-11-19 14:01:51 +0100

    moved date field tests

--- luxjs/tests/forms.js
+++ luxjs/tests/forms.js
@@ -128,7 +128,7 @@
         }));
     });
 
-    describe("Test form with file field", function() {
+    describe("Test lux.form with file field", function() {
         lux.formTests = {};
 
         beforeEach(function () {
@@ -164,6 +164,29 @@
             expect(tags[1].tagName).toBe('INPUT');
             expect(tags[1].getAttribute('type')).toBe('file');
             expect(tags[1].hasAttribute('ngf-select')).toBeFalsy();
+            //
+        }));
+    });
+
+    describe("Test lux.form with date field", function() {
+        lux.formTests = {};
+
+        beforeEach(function () {
+            module('lux.form');
+        });
+
+        it("adds the form-date directive", inject(function($compile, $rootScope) {
+            lux.formTests.date = testFormUtils.createForm([{type: 'date', name: 'date'}]);
+            var element = testFormUtils.digest($compile, $rootScope,
+                '<div><lux-form data-options="lux.formTests.date"></lux-form></div>');
+            //
+            var form = angular.element(element).find('form');
+            scope = form.scope();
+            scope.form.date.$setViewValue('2011-04-02');
+            scope.$digest();
+
+            expect(form.find('input')[0].hasAttribute('format-date')).toBeTruthy();
+            expect(scope.form.date.$modelValue instanceof Date).toBeTruthy();
             //
         }));
     });
