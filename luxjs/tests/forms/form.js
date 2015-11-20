652ed00b83a5b2ac27aa2c278d92c4bae1ca6dc9
tazo <tazo90@gmail.com>
2015-11-20 11:27:50 +0100

    fix tests

--- luxjs/tests/forms/form.js
+++ luxjs/tests/forms/form.js
@@ -54,14 +54,14 @@ define(function(require) {
         });
 
         it("basic input tests", inject(function($compile, $rootScope) {
-            lux.formTests.simple = testFormUtils.createForm({
+            lux.formTests.vForm1 = testFormUtils.createForm({
                 type: 'email',
                 name: 'login',
                 required: true
             });
 
             var element = testFormUtils.digest($compile, $rootScope,
-                '<div><lux-form data-options="lux.formTests.simple"></lux-form></div>'),
+                '<div><lux-form data-options="lux.formTests.vForm1"></lux-form></div>'),
                 form = element.children();
             //
             var tags = form.children().children();
@@ -74,14 +74,14 @@ define(function(require) {
         }));
 
         it("on success, email validate", inject(function($compile, $rootScope) {
-            lux.formTests.simple = testFormUtils.createForm({
+            lux.formTests.vForm2 = testFormUtils.createForm({
                 type: 'email',
                 name: 'login',
                 required: true
             });
 
             var element = testFormUtils.digest($compile, $rootScope,
-                '<div><lux-form data-options="lux.formTests.simple"></lux-form></div>');
+                '<div><lux-form data-options="lux.formTests.vForm2"></lux-form></div>');
 
             var form = angular.element(element).find('form');
             scope = form.scope();
@@ -92,14 +92,14 @@ define(function(require) {
         }));
 
         it("on failure, email validate", inject(function($compile, $rootScope) {
-            lux.formTests.simple = testFormUtils.createForm({
+            lux.formTests.vForm3 = testFormUtils.createForm({
                 type: 'email',
                 name: 'login',
                 required: true
             });
 
             var element = testFormUtils.digest($compile, $rootScope,
-                '<div><lux-form data-options="lux.formTests.simple"></lux-form></div>');
+                '<div><lux-form data-options="lux.formTests.vForm3"></lux-form></div>');
 
             var form = angular.element(element).find('form');
             scope = form.scope();
@@ -110,14 +110,14 @@ define(function(require) {
         }));
 
         it('submit empty email field', inject(function($compile, $rootScope) {
-            lux.formTests.simple = testFormUtils.createForm({
+            lux.formTests.vForm4 = testFormUtils.createForm({
                 type: 'email',
                 name: 'login',
                 required: true
             });
 
             var element = testFormUtils.digest($compile, $rootScope,
-                '<div><lux-form data-options="lux.formTests.simple"></lux-form></div>');
+                '<div><lux-form data-options="lux.formTests.vForm4"></lux-form></div>');
 
             var _form = angular.element(element).find('form');
             var validators = _form.find('span');
@@ -132,27 +132,50 @@ define(function(require) {
             expect(validators.eq(2).hasClass('ng-hide')).toBe(true);
         }));
 
-        it('submit invalid email field', inject(function($compile, $rootScope) {
-            lux.formTests.simple = testFormUtils.createForm({
+        it('submit valid email field', inject(function($compile, $rootScope) {
+            lux.formTests.vForm5 = testFormUtils.createForm({
                 type: 'email',
                 name: 'login',
                 required: true
             });
 
             var element = testFormUtils.digest($compile, $rootScope,
-                '<div><lux-form data-options="lux.formTests.simple"></lux-form></div>');
+                '<div><lux-form data-options="lux.formTests.vForm5"></lux-form></div>');
 
             var _form = angular.element(element).find('form');
             var validators = _form.find('span');
             scope = _form.scope();
 
             // Submit invalid email (angular validation)
-            scope.form.login.$setViewValue('example.com');
+            scope.form.login.$setViewValue('test@example.com');
+            scope.form.$setSubmitted(true);
+            scope.$digest();
+            expect(validators.eq(0).hasClass('ng-hide')).toBe(true);
+            expect(validators.eq(1).hasClass('ng-hide')).toBe(true);
+            expect(validators.eq(2).hasClass('ng-hide')).toBe(true);
+        }));
+
+        it('submit invalid email field', inject(function($compile, $rootScope) {
+            lux.formTests.vForm6 = testFormUtils.createForm({
+                type: 'email',
+                name: 'login',
+                required: true
+            });
+
+            var element = testFormUtils.digest($compile, $rootScope,
+                '<div><lux-form data-options="lux.formTests.vForm6"></lux-form></div>');
+
+            var _form = angular.element(element).find('form');
+            var validators = _form.find('span');
+            scope = _form.scope();
+
+            // Submit invalid email (angular validation)
+            scope.form.login.$setViewValue('invalid_email');
             scope.form.$setSubmitted(true);
             scope.$digest();
             expect(validators.eq(0).hasClass('ng-hide')).toBe(true);
             expect(validators.eq(1).hasClass('ng-hide')).toBe(false);
-            expect(validators.eq(2).hasClass('ng-hide')).toBe(true);
+            expect(validators.eq(2).hasClass('ng-hide')).toBe(false);
         }));
 
         function createLuxMock(apiMock) {
