fa29a2d4d0cf2b5c3806a401172924b7b5fd0132
Luca Sbardella <luca@quantmind.com>
2015-11-24 09:24:27 +0000

    auth token test

--- tests/auth/__init__.py
+++ tests/auth/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 import lux
 
 from lux import forms
-from lux.extensions import odm
+from lux.extensions import odm, rest
 from lux.extensions.auth.views import UserCRUD, GroupCRUD, PermissionCRUD
 
 from tests.config import *  # noqa
@@ -19,6 +19,14 @@ EXTENSIONS = ['lux.extensions.base',
 API_URL = ''
 AUTHENTICATION_BACKENDS = ['lux.extensions.auth.TokenBackend',
                            'lux.extensions.auth.BrowserBackend']
+DEFAULT_PERMISSION_LEVELS = {
+    'secret': rest.NONE,
+    'objective': 40,
+    'objective:subject': 0,
+    'objective:deadline': 20,
+    'objective:outcome': 10
+}
+
 
 Model = odm.model_base('odmtest')
 
@@ -26,7 +34,11 @@ Model = odm.model_base('odmtest')
 class Extension(lux.Extension):
 
     def api_sections(self, app):
-        return [UserCRUD(), GroupCRUD(), PermissionCRUD(), ObjectiveCRUD()]
+        return [UserCRUD(),
+                GroupCRUD(),
+                PermissionCRUD(),
+                ObjectiveCRUD(),
+                SecretCRUD()]
 
 
 class Objective(Model):
@@ -38,6 +50,12 @@ class Objective(Model):
     created = Column(DateTime, default=datetime.utcnow)
 
 
+class Secret(Model):
+    id = Column(Integer, primary_key=True)
+    value = Column(String(250))
+    created = Column(DateTime, default=datetime.utcnow)
+
+
 class ObjectiveForm(forms.Form):
     subject = forms.CharField(required=False)
     deadline = forms.CharField(required=False)
@@ -45,5 +63,13 @@ class ObjectiveForm(forms.Form):
     done = forms.BooleanField(default=False)
 
 
+class SecretForm(forms.Form):
+    value = forms.CharField(required=False)
+
+
 class ObjectiveCRUD(odm.CRUD):
     _model = odm.RestModel('objective', ObjectiveForm)
+
+
+class SecretCRUD(odm.CRUD):
+    _model = odm.RestModel('secret', SecretForm)
--- tests/auth/sqlite.py
+++ tests/auth/sqlite.py
@@ -72,15 +72,7 @@ class AuthUtils:
 
 class TestSqlite(test.AppTestCase, AuthUtils):
     config_file = 'tests.auth'
-    config_params = {
-        'DATASTORE': 'sqlite://',
-        'DEFAULT_PERMISSION_LEVELS': {
-            'objective': 40,
-            'objective:subject': 0,
-            'objective:deadline': 20,
-            'objective:outcome': 10
-        }
-    }
+    config_params = {'DATASTORE': 'sqlite://'}
 
     su_credentials = {'username': 'bigpippo',
                       'password': 'pluto'}
@@ -459,3 +451,7 @@ class TestSqlite(test.AppTestCase, AuthUtils):
         '''Test the response when using a corrupted token
         '''
         token = yield from self._token()
+        request = yield from self.client.get('/secrets')
+        self.assertEqual(request.response.status_code, 403)
+        request = yield from self.client.get('/secrets', token=token)
+        self.assertEqual(request.response.status_code, 200)
