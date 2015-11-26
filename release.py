e62d26e4b19e8c03312caccf9e6634199460b272
Luca Sbardella <luca@quantmind.com>
2015-11-26 15:38:30 +0000

    Release 0.3.1

--- release.py
+++ release.py
@@ -1,7 +1,25 @@
 import os
+import json
 
 from pulsar.apps.release import ReleaseManager
 version_file = os.path.join(os.path.dirname(__file__), 'lux', '__init__.py')
+
+
+def before_commit(manager, release):
+    """Update the package.json version
+    """
+    import lux
+
+    manager.logger.info('Update package.json')
+
+    with open('package.json', 'r') as f:
+        pkg = json.loads(f.read())
+
+    pkg['version'] = lux.__version__
+    pkg['description'] = lux.__doc__
+
+    with open('package.json', 'w') as f:
+        f.write(json.dumps(pkg, indent=4))
 
 
 if __name__ == '__main__':
