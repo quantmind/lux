6d891fbb42633f669599e3094941f2906284587f
Luca Sbardella <luca@quantmind.com>
2016-01-04 09:23:21 +0000

    added sphinx processor for static site builder

--- lux/extensions/content/commands/build_static.py
+++ lux/extensions/content/commands/build_static.py
@@ -1,18 +1,9 @@
-from pulsar import Setting
-
 import lux
 from lux.extensions.content import CMS, static
 
 
 class Command(lux.Command):
-    option_list = (Setting('relative_url',
-                           ['--relative-url'],
-                           action="store_true",
-                           default=False,
-                           desc='Use relative urls rather than absolute ones. '
-                                'Useful during development.'),)
-
-    help = "create the static site"
+    help = "create the static site from content"
 
     def run(self, options):
         cms = self.app.cms
--- lux/extensions/content/static.py
+++ lux/extensions/content/static.py
@@ -1,7 +1,20 @@
 import os
+import shutil
 from unittest import mock
 
+from lux.extensions.base import MediaRouter
+from lux.utils.files import skipfile
+
 from .views import TextCRUD
+
+
+def dst_path(dirpath, filename, src, dst):
+    rel_path = os.path.relpath(dirpath, src)
+    if rel_path != '.':
+        dst = os.path.join(dst, rel_path)
+    if not os.path.isdir(dst):
+        os.makedirs(dst)
+    return os.path.join(dst, filename)
 
 
 def build(cms):
@@ -9,6 +22,9 @@ def build(cms):
     for middleware in cms.middleware():
         if isinstance(middleware, TextCRUD):
             yield from build_content(middleware, app)
+
+    if app.config.get('SERVE_STATIC_FILES'):
+        copy_assets(app)
 
 
 def build_content(middleware, app):
@@ -32,3 +48,25 @@ def build_content(middleware, app):
                 html = (b''.join(response.content)).decode(response.encoding)
                 with open(loc, 'w') as fp:
                     fp.write(html)
+
+
+def copy_assets(app):
+    location = app.config['STATIC_LOCATION']
+    path = app.config['MEDIA_URL']
+    if path:
+        path = path[:-1]
+        location = os.path.join(location, path[1:])
+    router = MediaRouter(path, app.meta.media_dir)
+    for name, path in router.extension_paths(app):
+        dst = os.path.join(location, name)
+        copy_files(path, dst)
+
+
+def copy_files(src, dst):
+    for dirpath, _, filenames in os.walk(src):
+        for filename in filenames:
+            if skipfile(filename):
+                continue
+            full_src = os.path.join(dirpath, filename)
+            full_dst = dst_path(dirpath, filename, src, dst)
+            shutil.copyfile(full_src, full_dst)
