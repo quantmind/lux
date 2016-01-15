77899662e08bd0ee0141318ba4e277453252ab7a
Jeremy Herr <jeremyherr@bmlltech.com>
2016-01-15 12:06:46 +0000

    Removed fake record generator that was left in for testing

--- luxjs/grid/data-providers/gridDataProviderWebsocket.js
+++ luxjs/grid/data-providers/gridDataProviderWebsocket.js
@@ -48,8 +48,6 @@ function gridDataProviderWebsocketFactory ($scope) {
                     type: 'update'
                 });
 
-                setTimeout(sendFakeRecordOnce.bind(this), 0); // TODO Remove this. It's a dummy status update for development.
-
             } else if (msg.event === 'columns-metadata') {
                 this._listener.onMetadataReceived(msg.data.data);
             }
@@ -60,87 +58,6 @@ function gridDataProviderWebsocketFactory ($scope) {
         this._sockJs.addListener(this._channel, onMessage.bind(this));
 
         this._sockJs.connect(onConnect.bind(this));
-
-        // TODO Remove this. It's a dummy status update for development.
-        var sendFakeRecordOnce = function() {
-            /* jshint validthis:true */
-            this._listener.onDataReceived({
-                total: 100,
-                result: [{
-                    args: "[]",
-                    eta: 1440517140003.5932,
-                    hostname: "gen25880@oxygen.bmll.local",
-                    kwargs: "{}",
-                    name: "bmll.server_status",
-                    status: "sent",
-                    timestamp: 1440517140003.5932,
-                    uuid: "fa5b8e1b-2be7-4ec5-a7a6-f3c82db14117",
-                    result: "12:24:47 [p=20856, t=2828, ERROR, concurrent.futures] exception calling callback for <Future at 0x71854a8 state=finished raised RuntimeError>\n" +
-"Traceback (most recent call last):\n" +
-"  File \"c:\Python34\lib\concurrent\futures\thread.py\", line 54, in run\n" +
-"    result = self.fn(*self.args, **self.kwargs)\n" +
-"  File \"c:\Users\jeremy\Git\bmll\bmll-api\bmll\monitor\celeryapp.py\", line 179, in _receive_events\n" +
-"    self._next_event_call(self.polling_interval, self._receive_events)\n" +
-"  File \"c:\Users\jeremy\Git\bmll\bmll-api\bmll\monitor\celeryapp.py\", line 166, in _next_event_call\n" +
-"    callable)\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 462, in call_soon_threadsafe\n" +
-"    handle = self._call_soon(callback, args)\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 436, in _call_soon\n" +
-"    self._check_closed()\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 265, in _check_closed\n" +
-"    raise RuntimeError('Event loop is closed')\n" +
-"RuntimeError: Event loop is closed\n" +
-"\n" +
-"During handling of the above exception, another exception occurred:\n" +
-"\n" +
-"Traceback (most recent call last):\n" +
-"  File \"c:\Python34\lib\concurrent\futures\_base.py\", line 297, in _invoke_callbacks\n" +
-"    callback(self)\n" +
-"  File \"c:\Python34\lib\asyncio\futures.py\", line 408, in <lambda>\n" +
-"    new_future._copy_state, future))\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 462, in call_soon_threadsafe\n" +
-"    handle = self._call_soon(callback, args)\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 436, in _call_soon\n" +
-"    self._check_closed()\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 265, in _check_closed\n" +
-"    raise RuntimeError('Event loop is closed')\n" +
-"RuntimeError: Event loop is closed,\n" +
-"Traceback (most recent call last):\n" +
-"  File \"c:\Python34\lib\concurrent\futures\thread.py\", line 54, in run\n" +
-"    result = self.fn(*self.args, **self.kwargs)\n" +
-"  File \"c:\Users\jeremy\Git\bmll\bmll-api\bmll\monitor\celeryapp.py\", line 179, in _receive_events\n" +
-"    self._next_event_call(self.polling_interval, self._receive_events)\n" +
-"  File \"c:\Users\jeremy\Git\bmll\bmll-api\bmll\monitor\celeryapp.py\", line 166, in _next_event_call\n" +
-"    callable)\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 462, in call_soon_threadsafe\n" +
-"    handle = self._call_soon(callback, args)\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 436, in _call_soon\n" +
-"    self._check_closed()\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 265, in _check_closed\n" +
-"    raise RuntimeError('Event loop is closed')\n" +
-"RuntimeError: Event loop is closed\n" +
-"\n" +
-"During handling of the above exception, another exception occurred:\n" +
-"\n" +
-"Traceback (most recent call last):\n" +
-"  File \"c:\Python34\lib\concurrent\futures\_base.py\", line 297, in _invoke_callbacks\n" +
-"    callback(self)\n" +
-"  File \"c:\Python34\lib\asyncio\futures.py\", line 408, in <lambda>\n" +
-"    new_future._copy_state, future))\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 462, in call_soon_threadsafe\n" +
-"    handle = self._call_soon(callback, args)\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 436, in _call_soon\n" +
-"    self._check_closed()\n" +
-"  File \"c:\Python34\lib\asyncio\base_events.py\", line 265, in _check_closed\n" +
-"    raise RuntimeError('Event loop is closed')\n" +
-"RuntimeError: Event loop is closed,\n"
-                }],
-                type: 'update'
-            });
-
-        sendFakeRecordOnce = function() {};
-        };
-
     };
 
     GridDataProviderWebsocket.prototype.getPage = function(options) {
