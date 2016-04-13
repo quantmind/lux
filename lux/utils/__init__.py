73f065bf8f405066a31ec84d9c428ef6a7a1ea95
Luca Sbardella <luca@quantmind.com>
2016-04-13 22:57:17 +0100

    removed unused code

--- lux/utils/__init__.py
+++ lux/utils/__init__.py
@@ -1,52 +1,6 @@
-from collections import Hashable
-from functools import partial
 from urllib.parse import urlsplit
 
 from pulsar.utils.httpurl import is_absolute_uri
-
-
-class memoized(object):
-    """Function decorator to cache return values.
-
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-
-    """
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        if not isinstance(args, Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-
-    def __repr__(self):
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return partial(self.__call__, obj)
-
-
-def version_tuple(version):
-    bits = version.split('-')
-    version = bits[0].split('.')
-    assert len(version) == 3
-    version = list(map(int, version))
-    if len(bits) == 2:
-        rel = bits[1].split('.')
-        assert len(rel) == 2
-        version.append(rel[0])
-        version.append(int(rel[1]))
-    return tuple(version)
 
 
 def is_url(url):
