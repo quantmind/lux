465b93e1721db3b0f52194242cd916053960d6a4
Luca Sbardella <luca@quantmind.com>
2016-11-04 21:55:18 +0000

    removed js tests

--- example/js/app.js
+++
@@ -1,9 +0,0 @@
-require([
-    'require.config',
-    'angular',
-    '../../build/lux'
-], function(lux, angular) {
-    'use strict';
-
-    angular.bootstrap('website', ['lux.form', 'lux.cms', 'lux.nav']);
-});
