924adb0affaff9de261ab77abac218f66a1d3c92
Luca Sbardella <luca@quantmind.com>
2016-11-30 21:33:59 +0000

    removed obsolete test

--- tests/core/test_wrappers.py
+++ tests/core/test_wrappers.py
@@ -1,5 +1,3 @@
-from pulsar import ImproperlyConfigured
-
 from lux.utils import test
 
 
@@ -7,19 +5,10 @@ class TestWrappers(test.TestCase):
     config_file = 'tests.core'
 
     def test_is_secure(self):
-        app = self.application(SECURE_PROXY_SSL_HEADER=(
-            'HTTP_X_FORWARDED_PROTO', 'https'))
+        app = self.application()
         client = test.TestClient(app)
         request, _ = client.request_start_response('get', '/')
         self.assertFalse(request.is_secure)
-
-    def test_is_secure_improperly_configured(self):
-        app = self.application(
-            SECURE_PROXY_SSL_HEADER='HTTP_X_FORWARDED_PROTO')
-        client = test.TestClient(app)
-        request, _ = client.request_start_response('get', '/')
-        self.assertRaises(ImproperlyConfigured,
-                          lambda: request.is_secure)
 
     def test_logger(self):
         app = self.application()
